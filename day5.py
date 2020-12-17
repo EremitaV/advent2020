33d3510ba8cc02b672ed525491d884725db93bca
mathiaslauritsen <thamatt3390@gmail.com>
2020-12-18 00:26:41 +0100

    clean up

--- day5.py
+++ day5.py
@@ -7,15 +7,12 @@ part1 = 0
 nums = []
 for bp in board_passes:
 	row = range(0,128)
-	print("bp[:7]", bp[:7])
 	for c in bp[:7]:
-		print(row)
 		if c == 'F':
 			row = row[:len(row)//2]
 		else:
 			row = row[len(row)//2:]
 	row = row[0]
-	print("row", row)
 	seat = range(0,8)
 	for c in bp[7:]:
 		if c == 'L':
@@ -23,12 +20,12 @@ for bp in board_passes:
 		else:
 			seat = seat[len(seat)//2:]
 	seat = seat[0]
-	print("seat", seat)
 	ID = row*8 + seat
 	nums.append(ID)
 	part1 = max(part1,ID)
-	print("ID", ID)
+	print(f"row {row} seat {seat} ID {ID}")
+	
 print("part one", part1)
 all_seats = list(range(0,part1))
-part2 = set(all_seats)-set(nums)
+part2 = max(set(all_seats)-set(nums))
 print("part two", part2)
