b437e6e311d12e6ae2b156a5a3d65c98bde91d39
mathiaslauritsen <thamatt3390@gmail.com>
2020-12-18 00:34:51 +0100

    clean up

--- day5.py
+++ day5.py
@@ -1,19 +1,17 @@
 with open("inputs/day5.txt") as file: 
 	board_passes  = [bp.strip() for bp in file]
 
-#each letter tells us if we are in higher half or lower half. 
-
 part1 = 0
 nums = []
 for bp in board_passes:
-	row = range(0,128)
+	row = range(0,128) # [0,1,...,127]
 	for c in bp[:7]:
 		if c == 'F':
 			row = row[:len(row)//2]
 		else:
 			row = row[len(row)//2:]
 	row = row[0]
-	seat = range(0,8)
+	seat = range(0,8) # [0,1,...,7]
 	for c in bp[7:]:
 		if c == 'L':
 			seat = seat[:len(seat)//2]
@@ -24,7 +22,7 @@ for bp in board_passes:
 	nums.append(ID)
 	part1 = max(part1,ID)
 	print(f"row {row} seat {seat} ID {ID}")
-	
+
 print("part one", part1)
 all_seats = list(range(0,part1))
 part2 = max(set(all_seats)-set(nums))
